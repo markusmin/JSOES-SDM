bf3089e76aa9e691a27723492be5b7337407f9fd
Markus Min <markus.min@gmail.com>
2025-01-10 13:22:21 -0800

    added content to website, incl survey descriptions

--- docs/update_website.R
+++ docs/update_website.R
@@ -3,3 +3,6 @@
 rmarkdown::render(here::here("docs", "index.Rmd"))
 rmarkdown::render(here::here("docs", "project_description.Rmd"))
 rmarkdown::render(here::here("docs", "bongo_exploratory.Rmd"))
+rmarkdown::render(here::here("docs", "jsoes_trawl_exploratory.Rmd"))
+rmarkdown::render(here::here("docs", "CCES_survey_description.Rmd"))
+rmarkdown::render(here::here("docs", "PRS_survey_description.Rmd"))
