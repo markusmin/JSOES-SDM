3d7f7d6f2a35ce21ebb00cf31654d699848043c6
Markus Min <markus.min@gmail.com>
2025-01-18 07:36:17 -0800

    website restructuring

--- docs/update_website.R
+++ docs/update_website.R
@@ -1,6 +1,7 @@
 # Update website
 
 rmarkdown::render(here::here("docs", "index.Rmd"))
+rmarkdown::render(here::here("docs", "project_background.Rmd"))
 rmarkdown::render(here::here("docs", "project_description.Rmd"))
 rmarkdown::render(here::here("docs", "bongo_exploratory.Rmd"))
 rmarkdown::render(here::here("docs", "jsoes_trawl_exploratory.Rmd"))
