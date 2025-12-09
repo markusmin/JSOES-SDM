f35e48382bef42c57fae10efea44e9088b11e067
Markus Min <markus.min@gmail.com>
2025-12-09 15:50:03 -0800

    functions to generate maps with samples plotted

--- R/functions/plot_distribution_jsoes_seabirds.R
+++ R/functions/plot_distribution_jsoes_seabirds.R
@@ -9,12 +9,15 @@ plot_distribution_jsoes_seabirds <- function(data, taxon_name){
     geom_point(data = data, aes(x = dec_long, y = dec_lat, size = n_per_km2, color = encounter),
                alpha = 0.5) +
     scale_color_manual(values = c("zero" = "#fc9272", "non-zero" = "#2ca25f")) +
+    guides(size = guide_legend(title = "Density (N/km^2)"),
+           color = guide_legend(title = "Encounter")) +
     facet_wrap(~year, nrow = 3) +
     ggtitle(taxon_name) +
     theme(legend.position = "right",
           legend.key.height = unit(0.35, "cm"),
           legend.key.width = unit(0.25, "cm"),
           legend.title = element_text(size = 8),
+          axis.title.x = element_blank(),
           legend.text = element_text(size = 6),
           legend.spacing.y = unit(0.01, 'cm')) -> species_distribution_plot
   
